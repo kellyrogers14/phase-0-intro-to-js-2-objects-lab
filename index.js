0b930bc420ff58389d72b21f788886229d8fc376
Kelly Rogers <kellyerogers92@gmail.com>
2023-11-14 16:04:11 -0700

    complete

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution in this file!
+const employee = {
+    name: "Kelly",
+    streetAddress: "4157 Perry St" 
+}
+
+//
+
+function updateEmployeeWithKeyAndValue (employee, key, value){
+    const newEmployee = {
+        ...employee,
+        name: "Sam",
+        streetAddress: "11 Broadway"
+    };
+
+    return newEmployee
+};
+
+//
+
+function destructivelyUpdateEmployeeWithKeyAndValue (employee, key, value){
+   employee.streetAddress = "12 Broadway";
+   employee.name = "Sam";
+   return employee
+};
+
+//
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = {...employee};
+    delete newEmployee.name;
+    return newEmployee;
+};
+
+//
+
+function destructivelyDeleteFromEmployeeByKey (employee, key) {
+    delete employee.name;
+    return employee;
+}
